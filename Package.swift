77cfa948bf5a562669fcd989cfa8ebb0266d0b89
Diederick de Buck <diederick.de.buck@platform-42.com>
2024-09-03 20:44:56 +0200

    refactor in order to package

--- Package.swift
+++ Package.swift
@@ -8,14 +8,11 @@ let package = Package(
         .watchOS(.v6)
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "P42-extensions",
             targets: ["P42-extensions"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "P42-extensions"),
         .testTarget(
