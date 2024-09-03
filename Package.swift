2a09dc2688b54e1383ab070d440ba2e391f8a9ed
Diederick de Buck <diederick.de.buck@platform-42.com>
2024-09-03 10:47:16 +0200

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "P42-extensions",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "P42-extensions",
+            targets: ["P42-extensions"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "P42-extensions"),
+        .testTarget(
+            name: "P42-extensionsTests",
+            dependencies: ["P42-extensions"]),
+    ]
+)
