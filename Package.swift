e3927c0c33fd1225a168d8734fa586e4988bb466
Diederick de Buck <diederick.de.buck@platform-42.com>
2024-09-03 12:13:38 +0200

    min version spec

--- Package.swift
+++ Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "P42-extensions",
+    platforms: [
+        .iOS(.v14), 
+        .watchOS(.v6)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
