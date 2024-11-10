421c32629f441d35a9716e4252244cc1d0b1aa4f
Diederick de Buck <diederick.de.buck@platform-42.com>
2024-11-10 10:02:57 +0100

    dddd

--- Sources/P42-extensions/View.swift
+++ Sources/P42-extensions/View.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+import SwiftUI
+
+extension View {
+    @available(iOS 14, *)
+    func navigationBarTitleTextColor(_ color: Color) -> some View {
+        let uiColor = UIColor(color)
+        UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: uiColor ]
+        UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: uiColor ]
+        return self
+    }
+}
